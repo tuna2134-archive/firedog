b40033c676f9eb6b179f42c571c48493ca07f75a
mc_fdc <Masato.04.11.2007@gmail.com>
2023-02-19 11:13:30 +0000

    ok

--- core/tree.py
+++ core/tree.py
@@ -5,6 +5,25 @@ from discord.app_commands import (
     CommandOnCooldown
 )
 import discord
+from traceback import TracebackException
+
+
+class ErrorView(discord.ui.View):
+
+    def __init__(self, error: AppCommandError):
+        super().__init__(timeout=180)
+        self.error = error
+
+    @discord.ui.button(label="詳細", emoji=":arrow_down:")
+    async def describe(
+        self, interaction: discord.Interaction, style: discord.ButtonStyle
+    ):
+        content = TracebackException.from_exception(self.error)
+        await interaction.response.send_message(embed=discord.Embed(
+            title="例外エラー2",
+            description=f"```py\n{str(content)}\n```0",
+            color=discord.Color.red()
+        ))
 
 
 class FiredogTree(CommandTree):
@@ -18,7 +37,7 @@ class FiredogTree(CommandTree):
                 description=f"負荷防止のため、{round(error.retry_after, 2)}"
                 "秒後にお試しください。",
                 color=discord.Color.red()
-            ))
+            ), ephemeral=True)
         else:
             await interaction.response.send_message(embed=discord.Embed(
                 title="例外エラー",
