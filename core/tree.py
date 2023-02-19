bb95b7e5524d0cbe32ea0edb3eb7cbdd9098db44
mc_fdc <Masato.04.11.2007@gmail.com>
2023-02-19 10:41:10 +0000

    ok

--- core/tree.py
+++ core/tree.py
@@ -1,5 +1,9 @@
 # Core - tree
-from discord.app_commands import CommandTree, AppCommandError
+from discord.app_commands import (
+    CommandTree,
+    AppCommandError,
+    CommandOnCooldown
+)
 import discord
 
 
@@ -8,8 +12,15 @@ class FiredogTree(CommandTree):
     async def on_error(
         self, interaction: discord.Interaction, error: AppCommandError
     ) -> None:
-        await interaction.response.send_message(embed=discord.Embed(
-            title="例外エラー",
-            description=error,
-            color=discord.Color.red()
-        ))
+        if isinstance(error, CommandOnCooldown):
+            await interaction.response.send_message(embed=discord.Embed(
+                title="クールダウンエラー",
+                description=f"負荷防止のため、{error.retry_after}秒後にお試しください。",
+                color=discord.Color.red()
+            ))
+        else:
+            await interaction.response.send_message(embed=discord.Embed(
+                title="例外エラー",
+                description=error,
+                color=discord.Color.red()
+            ))
