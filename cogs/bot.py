1cc408b3f9864dfc970cf86da6eade45efe3fed7
mc_fdc <Masato.04.11.2007@gmail.com>
2023-02-19 00:11:03 +0000

    ok

--- cogs/bot.py
+++ cogs/bot.py
@@ -11,7 +11,7 @@ class Bot(commands.Cog):
         self.bot = bot
         self.pool = bot.pool
 
-    @tasks.sleep(minutes=2.0)
+    @tasks.loop(minutes=2.0)
     async def change_presence(self) -> None:
         await self.bot.change_presence(
             status=discord.Status.online, activity=discord.Activity(
